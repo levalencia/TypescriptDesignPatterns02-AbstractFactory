a9b36d3262eb4562738cbf6135b68853a4f9e987
Luis Valencia <luisevalencia@gmail.com>
2018-01-03 16:27:58 +0100

    changes

--- src/webparts/typescriptDesignPatterns02AbstractFactory/components/TypescriptDesignPatterns02AbstractFactory.tsx
+++ src/webparts/typescriptDesignPatterns02AbstractFactory/components/TypescriptDesignPatterns02AbstractFactory.tsx
@@ -1,37 +1,25 @@
-import * as React from 'react';
-import styles from './TypescriptDesignPatterns02AbstractFactory.module.scss';
-import { ITypescriptDesignPatterns02AbstractFactoryProps } from './ITypescriptDesignPatterns02AbstractFactoryProps';
-import { escape } from '@microsoft/sp-lodash-subset';
-import { ITypescriptDesignPatterns02AbstractFactoryState } from './ITypescriptDesignPatterns02AbstractFactoryState';
-import SharepointListDAOFactory from './Factory/SharepointListDAOFactory';
-import DAOFactory from './Factory/DAOFactory';
-import ICustomerDAO from './Factory/ICustomerDAO';
-import DataSources from './Factory/DatasourcesEnum';
-import Customer from './Factory/Customer';
+import * as React from "react";
+import { ITypescriptDesignPatterns02AbstractFactoryProps } from "./ITypescriptDesignPatterns02AbstractFactoryProps";
+import { ITypescriptDesignPatterns02AbstractFactoryState } from "./ITypescriptDesignPatterns02AbstractFactoryState";
+import DAOFactory from "./Factory/DAOFactory";
+import ICustomerDAO from "./Factory/ICustomerDAO";
+import DataSources from "./Factory/DatasourcesEnum";
 
-export default class TypescriptDesignPatterns02AbstractFactory extends React.Component<ITypescriptDesignPatterns02AbstractFactoryProps, ITypescriptDesignPatterns02AbstractFactoryState> {
+export default class TypescriptDesignPatterns02AbstractFactory extends 
+React.Component<ITypescriptDesignPatterns02AbstractFactoryProps, ITypescriptDesignPatterns02AbstractFactoryState> {
+  private customerDao: ICustomerDAO;
+
   constructor(props: ITypescriptDesignPatterns02AbstractFactoryProps, state: ITypescriptDesignPatterns02AbstractFactoryState) {
     super(props);
     this.setInitialState();
+    this.setDaos(props.datasource);
   }
 
   public render(): React.ReactElement<ITypescriptDesignPatterns02AbstractFactoryProps> {
-    switch(this.props.datasource) {
-      case "Sharepoint":
-        let sharepointlistdaofactory: SharepointListDAOFactory =   DAOFactory.getDAOFactory(DataSources.SharepointList);
-        let customerSharepointDAO: ICustomerDAO = sharepointlistdaofactory.getCustomerDAO();
-        this.state = {
-          items: customerSharepointDAO.listCustomers()
-        };
-        break;
-      case "JSON":
-        let jsondaofactory: SharepointListDAOFactory =   DAOFactory.getDAOFactory(DataSources.JsonData);
-        let customerJsonDAO: ICustomerDAO = jsondaofactory.getCustomerDAO();
-        this.state = {
-          items: customerJsonDAO.listCustomers()
-        };
-        break;
-    }
+    this.state = {
+        items: this.customerDao.listCustomers(),
+    };
+
     return null;
   }
 
@@ -41,6 +29,8 @@ export default class TypescriptDesignPatterns02AbstractFactory extends React.Com
     };
   }
 
-
-
+  private setDaos(datasource): void {
+    const data: any = datasource === "Sharepoint" ? DataSources.SharepointList : DataSources.JsonData;
+    this.customerDao = DAOFactory.getDAOFactory(data).getCustomerDAO();
+  }
 }
